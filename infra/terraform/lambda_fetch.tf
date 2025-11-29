3e775e629c86f3482c44e44098580157a1f8b90f
Adam <adam@gatherer.tech>
2025-11-29 18:29:09 +0000

    fixed typo

--- infra/terraform/lambda_fetch.tf
+++ infra/terraform/lambda_fetch.tf
@@ -1,5 +1,5 @@
 data "archive_file" "fetch_weather_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../../../lambdas/fetch_weather"
+  source_dir  = "${path.module}/../../lambdas/fetch_weather"
   output_path = "${path.module}/build/fetch_weather.zip"
 }
