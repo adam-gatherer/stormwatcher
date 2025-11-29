8981411d28772377c50d8317f0c49f9cacbb533a
Adam <adam@gatherer.tech>
2025-11-29 18:26:49 +0000

    moved protoypes scripts, created lambda_fetch.tf

---
+++ infra/terraform/lambda_fetch.tf
@@ -0,0 +1,5 @@
+data "archive_file" "fetch_weather_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../../lambdas/fetch_weather"
+  output_path = "${path.module}/build/fetch_weather.zip"
+}
