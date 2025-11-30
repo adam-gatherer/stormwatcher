a695d26d5f6efb450e98b7702f13d42ecd0bdb30
Adam <adam@gatherer.tech>
2025-11-30 13:46:47 +0000

    added sns subcriptions

--- infra/terraform/sns.tf
+++ infra/terraform/sns.tf
@@ -8,3 +8,19 @@ resource "aws_sns_topic" "storm" {
   name = "${var.project_name}-storm"
 }
 
+# sns subscriptions
+resource "aws_sns_topic_subscription" "status_email" {
+  count = var.status_email == "" ? 0 : 1
+
+  topic_arn = aws_sns_topic.status.arn
+  protocol  = "email"
+  endpoint  = var.status_email
+}
+
+resource "aws_sns_topic_subscription" "storm_email" {
+  count = var.storm_email == "" ? 0 : 1
+
+  topic_arn = aws_sns_topic.storm.arn
+  protocol  = "email"
+  endpoint  = var.storm_email
+}
